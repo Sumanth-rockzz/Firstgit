7884a970b652108e090c2e219944004503327563
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-01-19 20:24:36 +0530

    dom project basics

---
+++ dom.js
@@ -0,0 +1,9 @@
+/* console.log(document.getElementById('main-header'));
+const header=document.getElementById('main-header');
+console.log(header.innerText);
+console.log(header.textContent); */
+const header=document.getElementById('main-header');
+header.style.borderBottom='2px solid black';
+const additem=document.querySelector('h2');
+additem.style.fontWeight='bold';
+additem.style.color='green';
