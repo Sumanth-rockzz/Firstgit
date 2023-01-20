7faef320fb26e861f3922fb719e9ad3fc90469cc
Sumanth-rockzz <Sumanthn876@gmail.com>
2023-01-21 04:04:28 +0530

    task 8 delele and edit

--- dom.js
+++ dom.js
@@ -1,147 +1,86 @@
-/* console.log(document.getElementById('main-header'));
-const header=document.getElementById('main-header');
-console.log(header.innerText);
-console.log(header.textContent); */
-/* const header=document.getElementById('main-header');
-header.style.borderBottom='2px solid black';
-const additem=document.querySelector('h2');
-additem.style.fontWeight='bold';
-additem.style.color='green'; */
-/* const listitems =document.getElementsByClassName("list-group-item");
-console.log(listitems);
-console.log(listitems[1]);
-listitems[1].innerText='hello';
-listitems[1].style.fontWeight='bold';
-listitems[1].style.backgroundColor='yellow';
+const form=document.getElementById('addForm');
+const itemlist=document.getElementById('items');
+//form submit event
+form.addEventListener('submit',additem);
 
-//listitems.style.backgroundColor='blue';  //it gives error
+//delete event
+itemlist.addEventListener('click',removeitem);
 
-for(let i=0;i<listitems.length;i++)
-{
-    listitems[i].style.color='maroon';
-    listitems[i].style.fontWeight='bold';
+//add item function
+function additem(e){
+    e.preventDefault();
+
+//get input value
+const newitem=document.getElementById('item');
+
+//crete li elemnt
+const li=document.createElement('li');
+
+//add class name
+li.className="list-group-item";
+ console.log(li);
+
+//add text node with input value
+li.appendChild(document.createTextNode(newitem.value));
+
+//creatte delete button
+const del=document.createElement('button');
+
+//add class name
+del.className='btn btn-danger btn-sm float-right delete';
+
+//append text node
+del.appendChild(document.createTextNode('x'));
+
+//append del btun to li
+li.appendChild(del);
+
+//append li to itemlist
+itemlist.appendChild(li);
 }
-listitems[2].style.backgroundColor='green'; //overide pev
- */
-/*  const li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-for(let i=0;i<li.length;i++)
-{
-    li[i].style.color='maroon';
-    li[i].style.backgroundColor='#f4f4f4';
-    li[i].style.fontWeight='bold';
-} 
-const listitems=document.getElementsByClassName('list-group-item');
-console.log(listitems);
-for(let i=0;i<listitems.length;i++)
-{
-   
-    listitems[i].style.backgroundColor='#f4f4f4';
-} */
-/* const li=document.querySelector('.list-group-item:nth-child(2)');
-li.style.backgroundColor='green';
-const itemli=document.querySelector('.list-group-item:nth-child(3)');
-itemli.style.display='none';
 
-
-const newli=document.querySelectorAll('li');
-newli[1].style.color="green";
-
-const odd=document.querySelectorAll('li:nth-child(odd)');
-for(let i=0;i<odd.length;i++)
+//remove item function
+function removeitem(e)
 {
-odd[i].style.backgroundColor="green";
+
+    if(e.target.classList.contains('delete'))
+    {
+
+        if(confirm('Are you sure?'))
+        {
+        const li=e.target.parentElement;
+        itemlist.removeChild(li);
+       
+    
+        }
+    }
+
 }
- */
-/* //parent node
-const itemlist=document.querySelector('#items');
-console.log(itemlist.parentNode);
-itemlist.parentNode.style.backgroundColor='#ccc';
-console.log(itemlist.parentNode.parentNode); */
+//select itemlistclass
 
-/* //parent element
-const itemlist=document.querySelector('#items');
-console.log(itemlist.parentElement);
-itemlist.parentElement.style.backgroundColor='#a4a4a4';
-console.log(itemlist.parentElement.parentElement); */
-const itemlist=document.querySelector('#items');
-/* //childnotes
-console.log(itemlist.childNodes);//includes linebreak
+const edititem=document.getElementsByClassName('list-group-item');
+for(let i=0;i<edititem.length;i++)
+{
+//create edit element
+const editbtn=document.createElement('button');
 
-//children
-console.log(itemlist.children);//only lists
-itemlist.children[1].style.backgroundColor='yellow';
+//create textnode
+editbtn.appendChild(document.createTextNode('Edit'));
 
+//add class name
+editbtn.className='btn btn-danger btn-sm float-right edit';
 
-//first child
-console.log(itemlist.firstChild);//includes line break or space
+//append inside edititem
 
-//first element child
-console.log(itemlist.firstElementChild) //only lists
-itemlist.firstElementChild.textContent='Hello 1';
-
-//last child
-console.log(itemlist.lastChild);//includes line break or space
-
-//LAST element child
-console.log(itemlist.lastElementChild) //only lists
-itemlist.lastElementChild.textContent='Hello 4'; */
-
-/* //nextsibling
-console.log(itemlist.nextSibling);
+edititem[i].appendChild(editbtn);
+}
 
 
-//next element sibling
-console.log(itemlist.nextElementSibling);//null becuse we dont have any next element
-
-//previoussilbling
-console.log(itemlist.previousSibling);
-
-//previousElementsilbling
-console.log(itemlist.previousElementSibling);
-itemlist.previousElementSibling.style.fontWeight='bold'; */
-
-//create element and insert
-
-//create a div
-const newdiv=document.createElement('div');
-
-//add class
-newdiv.className='hello';
-
-//add id
-newdiv.id='hello1';
-
-//add attribute
-
-newdiv.setAttribute('title','Hello div');
-
-//create a next node 
-const newdivtext=document.createTextNode('Hello world');
-
-//add text to div
-newdiv.appendChild(newdivtext);
-
-const container=document.querySelector('header .container');
-const h1=document.querySelector('header h1');
 
 
-console.log(newdiv);
 
-container.insertBefore(newdiv,h1);
 
-newdiv.style.fontSize='20px';
 
-//create a li and inert inside ul
 
-const newitem=document.createElement('li');
-newitem.classList.add('list-group-item')
 
-const newitemtext=document.createTextNode('hello world');
 
-newitem.appendChild(newitemtext);
-console.log(newitem);
-const add=document.querySelector('.list-group');
-const newone=document.querySelector('li');
-add.insertBefore(newitem,newone);
